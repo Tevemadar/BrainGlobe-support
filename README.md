9eb10b6a9d283564b01fd49f360f695861cad610
Gergely Csucs <Tevemadar@users.noreply.github.com>
2026-04-01 19:56:53 +0200

    makecutlas and docs

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # BrainGlobe support
 
+Python converters for using atlases from https://brainglobe.info with NeSys utilities (actual list of atlases is [here](https://brainglobe.info/documentation/brainglobe-atlasapi/usage/atlas-details.html)). So far a single one.
+
+## QuickNII cutlas creator
+
+Install BrainGlobe Atlas API if needed: 
+`pip install brainglobe-atlasapi`
+
+Supply atlas identifier as command-line parameter: 
+`python makecutlas.py allen_mouse_25um`
+
+Expected output is `allen_mouse_25um.cutlas` for the example. It then can be copied into any QuickNII release. 
+Launching the custom atlas with QuickNII:
+ - either as command-line parameter (e.g. `QuickNII.exe allen_mouse_25um.cutlas`)
+ - or edit `pack.txt` (next to QuickNII.exe) to contain the name of the new file (`allen_mouse_25um`). Without any trailing characters (not even a line-break).
+
+Tested with a couple atlases only, allen_mouse_25um, whs_sd_rat_39um, kim_dev_mouse_e18-5_lsfm_20um.
+
+Known limitations:
+
+ - QuickNII needs isotropic (or near-isotropic) resolution (it will load non-isotropic atlases, but they won't really work)
+ - atlases are unconditionally converted into a 4-volume format (2-byte template and 2-byte segmentation), that runs out of memory sooner than the theoretical limit (bit less than 2 gigavoxels)
+ - license information is not emitted yet
+ - single template - single segmentation.
---
+++ makecutlas.py
@@ -0,0 +1,111 @@
+import brainglobe_atlasapi, struct, sys, zlib
+
+print("Loading atlas")
+atlas = brainglobe_atlasapi.bg_atlas.BrainGlobeAtlas(sys.argv[1])
+(ydim, zdim, xdim) = atlas.shape
+print(f"Shape: {atlas.shape}")
+
+if xdim * ydim * zdim > 0x7FFFFFF0:
+    print("Atlas can't be packed into cutlas.")
+    sys.exit()
+
+print("Building annotations")
+palette = bytearray()
+remap = {0:0}
+for structure in atlas.structures_list:
+    id = structure["id"]
+    if id != 0:
+        remap[id] = len(remap)
+        name = structure["name"]
+        name = name.encode("utf-8")
+        name = struct.pack(f">H{len(name)}s", len(name), name)
+        palette.extend(struct.pack("3B", *structure["rgb_triplet"]))
+        palette.extend(name)
+palette = struct.pack(f">H{len(palette)}s", len(remap)-1, palette)
+palette = zlib.compress(palette, level = 9)
+palette = struct.pack(f">I{len(palette)}s", len(palette), palette)
+
+print("Building annotation image")
+annotation = atlas.annotation
+ahi = bytearray()
+alo = bytearray()
+for z in reversed(range(zdim)):
+    print(z, end = " \r")
+    for y in reversed(range(ydim)):
+        for x in reversed(range(xdim)):
+            v = remap[int(annotation[y, z, x])]
+            alo.append(v & 255)
+            ahi.append(v >> 8)
+
+print("Compressing annotation image")
+cahi = zlib.compress(ahi, level = 9)
+calo = zlib.compress(alo, level = 9)
+
+print("Building template image")
+template = atlas.reference
+minval = template.min()
+maxval = template.max()
+print(f"Range: {minval} - {maxval}")
+template = (template - minval) * 65535.0 / (maxval - minval)
+
+thi = bytearray()
+tlo = bytearray()
+for z in reversed(range(zdim)):
+    print(z, end = " \r")
+    for y in reversed(range(ydim)):
+        for x in reversed(range(xdim)):
+            v = int(template[y, z, x])
+            tlo.append(v & 255)
+            thi.append(v >> 8)
+
+print("Compressing template image")
+cthi = zlib.compress(thi, level = 9)
+ctlo = zlib.compress(tlo, level = 9)
+
+filename = atlas.atlas_name + ".cutlas"
+print("Writing " + filename)
+with open(filename, "wb") as f:
+    f.write(b"v0")
+
+    name = atlas.atlas_name
+    name = name.encode("utf-8")
+    name = struct.pack(f">H{len(name)}s", len(name), name)
+    f.write(name)
+
+    license = "<b>License placeholder</b>"
+    license = license.encode("utf-8")
+    license = struct.pack(f">H{len(license)}s", len(license), license)
+    license = zlib.compress(license, level = 9, wbits = -15)
+    license = struct.pack(f">I{len(license)}s", len(license), license)
+    f.write(license)
+
+    f.write(struct.pack(">HHH", xdim, ydim, zdim))
+
+    f.write(struct.pack("B", 2))
+
+    name = "Template"
+    name = name.encode("utf-8")
+    name = struct.pack(f">H{len(name)}s", len(name), name)
+    head = struct.pack(f">B{len(name)}sBdd", 2, name, 0, minval, maxval)
+    f.write(head)
+
+    name = "Segmentation"
+    name = name.encode("utf-8")
+    name = struct.pack(f">H{len(name)}s", len(name), name)
+    head = struct.pack(f">B{len(name)}sB", 4, name, 2)
+    f.write(head)
+    f.write(palette)
+
+    f.write(struct.pack("B", 0))
+
+    f.write(struct.pack("B", 4))
+
+    f.write(struct.pack(">I", len(cthi)))
+    f.write(cthi)
+    f.write(struct.pack(">I", len(ctlo)))
+    f.write(ctlo)
+
+    f.write(struct.pack(">I", len(cahi)))
+    f.write(cahi)
+    f.write(struct.pack(">I", len(calo)))
+    f.write(calo)
